dcf1b48aa320734cad3875afc17027be0c15624f
Tanmay Guchhait <tanmayguchhait@eurofins.com>
2019-02-28 16:52:57 +0530

    Ding Ding Project

---
+++ DingDing/DingDing/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DingDing.Startup))]
+namespace DingDing
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
